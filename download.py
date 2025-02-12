667dacf9797785c1d982f264f2effb9c162bb707
hp92467 <85086901+hp92467@users.noreply.github.com>
2025-02-12 13:12:35 +0800

    1

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
 import torch
 from modelscope import snapshot_download, AutoModel, AutoTokenizer
 from modelscope import GenerationConfig
-model_dir = snapshot_download('deepseek-ai/deepseek-llm-7b-chat', cache_dir='/root/autodl-tmp', revision='master')
+model_dir = snapshot_download('deepseek-ai/deepseek-llm-7b-chat', cache_dir='/home/wyb/hp/pycharm_projects/nlpcda/deepseek', revision='master')
